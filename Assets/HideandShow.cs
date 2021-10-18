6233ff2417eccecb79733a80c0aea39395749957
diab3tes <sm.shuvo496@gmail.com>
2021-10-18 19:52:16 +0600

    unity files

---
+++ Assets/HideandShow.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideandShow : MonoBehaviour
+{	
+    private Renderer _renderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _renderer = gameObject.GetComponent<Renderer>();
+    }
+    public void onEnter(GameObject obj)
+    {
+        obj.SetActive(true);
+    }
+
+    public void onExit(GameObject obj)
+    {
+        obj.SetActive(false);
+	
+    }
+    public void change(GameObject obj)
+    {
+       obj.SetActive(true);
+       gameObject.SetActive(false);
+
+    }	
+}
---
+++ Assets/PlayScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayScript : MonoBehaviour
+{
+    public AudioSource record;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void playSound()
+    {
+        record.Play();
+    }
+}
